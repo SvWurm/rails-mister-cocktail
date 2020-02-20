68e6c48729b63a1d776e39c3b4ae0ba0b0cf99b1
svenia wurm <svwu@outlook.com>
2020-02-20 15:08:54 +0100

    created dose and cocktail controller

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail
+
+  def create
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def doses_params
+    params.require(:dose).permit(:ingredient_id, :cocktail_id, :description)
+  end
+end
