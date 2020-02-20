b25493c807a2e66866d28ea1e63fb9edad11d0d9
svenia wurm <svwu@outlook.com>
2020-02-20 15:09:51 +0100

    created migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+buffer = open(url).read
+parse_website = JSON.parse(buffer)
+
+parse_website['drinks'].each do |ing|
+Ingredient.create(name: ing["strIngredient1"])
+end
